2294e5f1ac02540e65eef4182d6a675acd50d7ba
Arthur <arthur_2335@hotmail.com>
2022-05-12 20:19:44 -0400

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0) // lets react listen for variable changes
+  const [firstName, setFirstName] = useState('Art')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>You clicked the button {count} times.</h1>
+        <button onClick={() => setCount(count + 1)}>Addition</button>&nbsp;
+        <button className='btn-blue' onClick={() => setCount(0)}>REST</button>&nbsp;
+        <button className='btn-red' onClick={() => setCount(count ? count - 1 : 0)}>Subtraction</button>
+        {/* <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button> */}
+        <h2>{isLoggedIn ? 'Welcome Back' : 'Welcome Guest'}</h2>
+        <button className='btn-blue' onClick={loginLogout}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+
+        <section>
+        <h2 onClick={() => setFirstName('Arthur')}>Hello {firstName}.</h2>
+        </section>
       </header>
     </div>
   );
